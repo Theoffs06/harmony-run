2f2b288ef405593d78445004cc385f95cf76e963
TRISTAN <debeaunetristan@gmail.com>
2025-12-05 15:07:56 +0100

    improve jump trigger to execute autojump on jump input

--- Assets/Scripts/JumpTrigger.cs
+++ Assets/Scripts/JumpTrigger.cs
@@ -1,21 +1,34 @@
-using System.Collections;
-using System.Threading;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpTrigger : MonoBehaviour
 {
-    private PlayerJump playerJump;
+    private List<PlayerJump> playersJump = new();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerEntering = other.GetComponent<PlayerJump>();
+        playersJump.Add(playerEntering);
+        playerEntering.OnJumped += AutoJumpOnPlayerJump;
+    }
+
+    private void AutoJumpOnPlayerJump(PlayerJump player)
+    {
+        player.Jump();
+        playersJump.Remove(player);
+        player.OnJumped -= AutoJumpOnPlayerJump;
+    }
+
     private void OnTriggerExit(Collider other)
     {
+        var playerExiting = other.GetComponent<PlayerJump>();
+        if (playersJump.Contains(playerExiting))
         {
-            playerJump = other.GetComponent<PlayerJump>();
-            if (playerJump != null)
-            {
-                playerJump.Jump();
-            }
+            playerExiting.Jump();
+            playersJump.Remove(playerExiting);
+            playerExiting.OnJumped -= AutoJumpOnPlayerJump;
         }
     }
 }
