0bd913ec7396e3e7223f190aded153f77739e66f
Oscar Gaudin <oscar.gaudin@gmail.com>
2025-12-05 10:36:58 +0100

    Merged boost feature into dev

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Splines;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour {
@@ -14,21 +15,35 @@ public class PlayerMovement : MonoBehaviour {
 
     [Header("Safety")]
     [SerializeField] private LayerMask safetyMask;
-    
+
+    [Header("Boost")]
+    [SerializeField] private InputActionReference boostInput;
+    [SerializeField] private float boostSpeedMultiplier = 2.0f;
+    [SerializeField] private float boostConsumptionRate = 0.01f;
+
+    [Header("UI")]
+    [SerializeField] private Slider boostUI;
+
+    private InputAction _directionInput;
     private Rigidbody _rb;
 
     private float _input;
+    private bool _boostInput = false;
     private bool _forceRotation = true;
     
     private Vector3 _splineForward, _splineRight;
     private float3 _splineNearestPoint;
-    
     public SplineContainer TrackSpline { set => trackSpline = value; }
 
     private void Awake() {
+        _directionInput = InputSystem.actions.FindAction("Direction");
         _rb = GetComponent<Rigidbody>();
     }
-    
+
+    private void Update() {
+        HandlePlayerInput();
+    }
+
     private void FixedUpdate() {
         SplineUtility.GetNearestPoint(trackSpline.Spline, transform.position, out _splineNearestPoint, out var t);
         _splineForward = math.normalize(trackSpline.Spline.EvaluateTangent(t));
@@ -36,7 +51,10 @@ public class PlayerMovement : MonoBehaviour {
         
         Advance();
     }
-    
+
+    private void OnEnable() => _directionInput.Enable();
+    private void OnDisable() => _directionInput.Disable();
+
     private void OnCollisionEnter(Collision other) { 
         if (other.gameObject.layer == safetyMask) _forceRotation = false;
     }
@@ -44,14 +62,24 @@ public class PlayerMovement : MonoBehaviour {
     private void OnCollisionExit(Collision other) {
         if (other.gameObject.layer == safetyMask) _forceRotation = true;
     }
-    
-    public void OnMove(InputAction.CallbackContext obj) => _input = obj.ReadValue<float>();
-    
+
+    private void HandlePlayerInput() {
+        _input = _directionInput.ReadValue<float>();
+        _boostInput = boostInput.action.IsPressed();
+    }
+
     private void Advance() {
         if (_forceRotation) _rb.MoveRotation(Quaternion.FromToRotation(Vector3.forward, _splineForward));
         
         var verticalVelocity = _rb.linearVelocity.y;
-        var horizontalVelocity = _splineForward * speed + _splineRight * (_input * playerSteerSpeed);
+
+        var actualSpeed = speed;
+        if(_boostInput && boostUI.value > 0f)
+        {
+            actualSpeed = speed * boostSpeedMultiplier;
+            boostUI.value -= Time.fixedDeltaTime * boostConsumptionRate;
+        }
+        var horizontalVelocity = _splineForward * actualSpeed + _splineRight * (_input * playerSteerSpeed);
         _rb.linearVelocity = new Vector3(horizontalVelocity.x, verticalVelocity, horizontalVelocity.z);
     }
     
