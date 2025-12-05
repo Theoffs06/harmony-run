bbf94a3bf6b5c6a4f18daafd2cd46fca7ebd32ea
theoffs <theofontany.pro@gmail.com>
2025-12-05 15:15:56 +0100

    add chronometer

---
+++ Assets/Scripts/Chronometer.cs
@@ -0,0 +1,19 @@
+﻿public static class Chronometer {
+    private const int SecondsPerHour = 3600;
+    private const int SecondsPerMinute = 60;
+    private const int MillisecondsPerSecond = 100;
+    
+    private static float _elapsedTime;
+    
+    public static int Minutes => (int) (_elapsedTime % SecondsPerHour / SecondsPerMinute);
+    public static int Seconds => (int) (_elapsedTime % SecondsPerMinute);
+    public static int Milliseconds => (int) (_elapsedTime * MillisecondsPerSecond % MillisecondsPerSecond);
+    
+    public static void Update(float deltaTime) {
+        _elapsedTime += deltaTime;
+    }
+    
+    public static void Reset() {
+        _elapsedTime = 0;
+    }
+}
---
+++ Assets/Scripts/UI/UIChronometer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI {
+    public class UIChronometer : MonoBehaviour {
+        private TMP_Text _chronometerTxt;
+
+        private void Awake() {
+            _chronometerTxt = GetComponent<TMP_Text>();
+        }
+
+        private void Update() {
+            _chronometerTxt.SetText($"{Chronometer.Minutes:00}:{Chronometer.Seconds:00}:{Chronometer.Milliseconds:00}");
+            Chronometer.Update(Time.deltaTime);
+        }
+    }
+}
