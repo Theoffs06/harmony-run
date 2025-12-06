f3a4281e673b90fad1208b3b00f99be10c5ea7ae
TRISTAN <debeaunetristan@gmail.com>
2025-12-06 04:46:08 +0100

    make player UI

--- Assets/Scripts/DynamicSplitScreen.cs
+++ Assets/Scripts/DynamicSplitScreen.cs
@@ -1,36 +1,64 @@
 using UnityEngine;
 
-public class DynamicSplitScreen : MonoBehaviour {
-    [SerializeField] private Transform player1;
-    [SerializeField] private Transform player2;
-    
-    [SerializeField] private Camera singleCam;
-    [SerializeField] private Camera playerCam1;
-    [SerializeField] private Camera playerCam2;
-    [SerializeField] private GameObject splitVerticalImage;
-    [SerializeField] private GameObject splitHorizontalImage;
+public class DynamicSplitScreen : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player1;
 
-    [SerializeField] private GameObject playerListener1;
-    [SerializeField] private GameObject playerListener2;
+    [SerializeField]
+    private Transform player2;
 
-    [SerializeField] private float splitDistance = 10f;
-    [SerializeField] private float listenerDistance = 2f;
-    
+    [SerializeField]
+    private Camera singleCam;
+
+    [SerializeField]
+    private Camera playerCam1;
+
+    [SerializeField]
+    private Camera playerCam2;
+
+    [SerializeField]
+    private GameObject splitVerticalImage;
+
+    [SerializeField]
+    private GameObject splitHorizontalImage;
+
+    [SerializeField]
+    private GameObject playerListener1;
+
+    [SerializeField]
+    private GameObject playerListener2;
+
+    [SerializeField]
+    private float splitDistance = 10f;
+
+    [SerializeField]
+    private float listenerDistance = 2f;
+
     private bool _isSplit;
-    
+
+    public bool IsSplit => _isSplit;
+
+    public Camera SingleCam => singleCam;
+    public Camera PlayerCam1 => playerCam1;
+    public Camera PlayerCam2 => playerCam2;
+
     private static readonly Rect Left = new(0, 0, 0.5f, 1);
     private static readonly Rect Right = new(0.5f, 0, 0.5f, 1);
     private static readonly Rect Top = new(0, 0.5f, 1, 0.5f);
     private static readonly Rect Bottom = new(0, 0, 1, 0.5f);
 
-    private void Start() {
+    private void Start()
+    {
         SetSingleCamera();
     }
 
-    private void Update() {
+    private void Update()
+    {
         var shouldSplit = Vector3.Distance(player1.position, player2.position) > splitDistance;
 
-        switch (shouldSplit) {
+        switch (shouldSplit)
+        {
             case true when !_isSplit:
                 SetSplitCameras();
                 _isSplit = true;
@@ -42,8 +70,8 @@ public class DynamicSplitScreen : MonoBehaviour {
         }
     }
 
-
-    private void SetSingleCamera() {
+    private void SetSingleCamera()
+    {
         singleCam.gameObject.SetActive(true);
         playerCam1.gameObject.SetActive(false);
         playerCam2.gameObject.SetActive(false);
@@ -51,40 +79,49 @@ public class DynamicSplitScreen : MonoBehaviour {
         splitHorizontalImage.SetActive(false);
     }
 
-    private void SetSplitCameras() {
+    private void SetSplitCameras()
+    {
         singleCam.gameObject.SetActive(false);
         playerCam1.gameObject.SetActive(true);
         playerCam2.gameObject.SetActive(true);
         UpdateViewport();
     }
 
-    private void UpdateViewport() {
+    private void UpdateViewport()
+    {
         var dx = Mathf.Abs(player1.position.x - player2.position.x);
         var dy = Mathf.Abs(player1.position.y - player2.position.y);
-        
+
         var horizontalSplit = dx >= dy;
         splitVerticalImage.SetActive(horizontalSplit);
         splitHorizontalImage.SetActive(!horizontalSplit);
-        
-        if (horizontalSplit) {
-            if (player1.position.x < player2.position.x) {
+
+        if (horizontalSplit)
+        {
+            if (player1.position.x < player2.position.x)
+            {
                 playerListener1.transform.localPosition = Vector3.right * listenerDistance;
                 playerListener2.transform.localPosition = Vector3.left * listenerDistance;
                 SetRects(Left, Right);
             }
-            else {
+            else
+            {
                 playerListener1.transform.localPosition = Vector3.left * listenerDistance;
                 playerListener2.transform.localPosition = Vector3.right * listenerDistance;
                 SetRects(Right, Left);
             }
         }
-        else {
-            if (player1.position.y > player2.position.y) SetRects(Top, Bottom);
-            else SetRects(Bottom, Top);
+        else
+        {
+            if (player1.position.y > player2.position.y)
+                SetRects(Top, Bottom);
+            else
+                SetRects(Bottom, Top);
         }
     }
-    
-    private void SetRects(Rect playerRect1, Rect playerRect2) {
+
+    private void SetRects(Rect playerRect1, Rect playerRect2)
+    {
         playerCam1.rect = playerRect1;
         playerCam2.rect = playerRect2;
     }
---
+++ Assets/Scripts/UI/PlayerPositionUI.cs
@@ -0,0 +1,111 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerPositionUI : MonoBehaviour
+{
+    [SerializeField, Range(1, 2)]
+    private int playerCamNumber = 1;
+
+    [SerializeField]
+    private Transform playerToFollow;
+
+    [SerializeField]
+    private DynamicSplitScreen cameraManager;
+
+    [SerializeField]
+    private RectTransform rectTransform;
+
+    [SerializeField]
+    private Vector2 offset;
+
+    void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!cameraManager.IsSplit)
+        {
+            UpdatePositionFromCameraSingle(cameraManager.SingleCam);
+            rectTransform.gameObject.SetActive(true);
+        }
+        else
+        {
+            Camera cameraToCheck =
+                playerCamNumber == 1 ? cameraManager.PlayerCam1 : cameraManager.PlayerCam2;
+            UpdatePositionFromCameraSplited(cameraToCheck);
+
+            bool isVisible =
+                Vector3.Dot(
+                    cameraToCheck.transform.forward,
+                    playerToFollow.transform.position - cameraToCheck.transform.position
+                ) > 0f;
+
+            rectTransform.gameObject.SetActive(isVisible);
+        }
+    }
+
+    private void UpdatePositionFromCameraSingle(Camera camera)
+    {
+        Vector2 playerScreenPosition = camera.WorldToScreenPoint(playerToFollow.position);
+        Debug.Log("width :" + camera.pixelWidth);
+        Debug.Log("position " + playerToFollow.name + " : " + playerScreenPosition);
+
+        float xPosition = Mathf.Clamp(
+            playerScreenPosition.x - camera.pixelWidth / 2 + offset.x,
+            -camera.pixelWidth / 2,
+            camera.pixelWidth / 2
+        );
+        float yPosition = Mathf.Clamp(
+            playerScreenPosition.y - camera.pixelHeight / 2 + offset.y,
+            -camera.pixelHeight / 2,
+            camera.pixelHeight / 2
+        );
+
+        rectTransform.localPosition = new Vector2(xPosition, yPosition);
+    }
+
+    private void UpdatePositionFromCameraSplited(Camera camera)
+    {
+        Vector2 playerScreenPosition = camera.WorldToScreenPoint(playerToFollow.position);
+        Debug.Log("width :" + camera.pixelWidth);
+        Debug.Log("position " + playerToFollow.name + " : " + playerScreenPosition);
+
+        var camRect = camera.rect;
+        bool isLeft = camRect.x <= 0.01f;
+
+        float sign = isLeft ? -1 : 0.5f;
+
+        Debug.Log("left ? => " + isLeft);
+
+        float xPosition = Mathf.Clamp(
+            playerScreenPosition.x - camera.pixelWidth / 2 + offset.x,
+            -camera.pixelWidth / 2,
+            camera.pixelWidth / 2
+        );
+        float yPosition = Mathf.Clamp(
+            playerScreenPosition.y - camera.pixelHeight / 2 + offset.y,
+            -camera.pixelHeight / 2,
+            camera.pixelHeight / 2
+        );
+        xPosition = playerScreenPosition.x - camera.pixelWidth / 2 + offset.x;
+        yPosition = playerScreenPosition.y - camera.pixelHeight / 2 + offset.y;
+
+        if (!isLeft)
+        {
+            xPosition -= 3 * camera.pixelWidth / 4;
+        }
+
+        xPosition = isLeft
+            ? Mathf.Clamp(xPosition, -camera.pixelWidth / 2, camera.pixelWidth / 2)
+            : Mathf.Clamp(xPosition, -camera.pixelWidth / 4, 3 * camera.pixelWidth / 4);
+
+        yPosition = Mathf.Clamp(yPosition, -camera.pixelHeight / 2, camera.pixelHeight / 2);
+
+        rectTransform.localPosition =
+            new Vector2(xPosition, yPosition) + sign * Vector2.right * camera.pixelWidth / 2;
+
+        // rectTransform.localPosition =
+        //     new Vector2(xPosition, yPosition) + sign * Vector2.right * camera.pixelWidth / 4;
+    }
+}
