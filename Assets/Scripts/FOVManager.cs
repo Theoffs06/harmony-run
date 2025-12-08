16ac924a29e6d21af2b2840b734abe5d6b805ce0
TRISTAN <debeaunetristan@gmail.com>
2025-12-08 21:23:29 +0100

    FOV effect when going fast

---
+++ Assets/Scripts/FOVManager.cs
@@ -0,0 +1,32 @@
+using Player;
+using Unity.Cinemachine;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class FOVManager : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerMovement playerMovement;
+
+    [SerializeField]
+    private CinemachineCamera camera;
+
+    [SerializeField]
+    private float FOVMinSpeed;
+
+    [SerializeField]
+    private float FOVMaxSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerSpeed = playerMovement.GetComponent<Rigidbody>().linearVelocity.magnitude;
+        float FOVFactor = math.clamp(math.remap(60f, 120f, 0f, 1f, playerSpeed), 0f, 1f);
+        Debug.Log("velocity => " + playerSpeed + " // factor => " + FOVFactor);
+        float targetFOV = Mathf.Lerp(FOVMinSpeed, FOVMaxSpeed, FOVFactor);
+        camera.Lens.FieldOfView = Mathf.Lerp(camera.Lens.FieldOfView, targetFOV, 0.1f);
+    }
+}
