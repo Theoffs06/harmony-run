d765c26eccbae1b4eb7337f35c08c73bf05ee245
TRISTAN <debeaunetristan@gmail.com>
2025-12-08 22:40:27 +0100

    add player tilt

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,45 +3,69 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Splines;
 
-namespace Player {
+namespace Player
+{
     [RequireComponent(typeof(Rigidbody))]
-    public class PlayerMovement : MonoBehaviour {
+    public class PlayerMovement : MonoBehaviour
+    {
         [Header("Spline Driving")]
-        [SerializeField] private SplineContainer trackSpline;
-        [SerializeField] private float speed = 30f;
+        [SerializeField]
+        private SplineContainer trackSpline;
+
+        [SerializeField]
+        private float speed = 30f;
 
         [Header("Player Steering")]
-        [SerializeField] private float playerSteerSpeed = 100f;
-        [SerializeField] private float maxSteerLength;
+        [SerializeField]
+        private float playerSteerSpeed = 100f;
+
+        [SerializeField]
+        private float maxSteerLength;
 
         [Header("Safety")]
-        [SerializeField] private LayerMask safetyMask;
-        
+        [SerializeField]
+        private LayerMask safetyMask;
+
         [Header("Boost")]
-        [SerializeField] private float boostSpeedMultiplier = 2.0f;
-        [SerializeField] private float proximityBoostMultiplier = 1.0f;
-        
+        [SerializeField]
+        private float boostSpeedMultiplier = 2.0f;
+
+        [SerializeField]
+        private float proximityBoostMultiplier = 1.0f;
+
         [Header("Brake")]
-        [SerializeField] private float brakeStrength = 20f;
-        
+        [SerializeField]
+        private float brakeStrength = 20f;
+
         private PlayerAudio _audio;
         private PlayerBoost _boost;
         private PlayerGround _ground;
         private Rigidbody _rb;
         private ProximitySpeedBoost _proximitySpeedBoost;
-        
+
         private UISpeedBar _speedUI;
-        
+
         private bool _forceRotation = true;
-    
-        private float3 _splineForward, _splineRight;
+
+        private float3 _splineForward,
+            _splineRight;
         private float3 _splineNearestPoint;
         private float3 _rightMaxSteerPoint;
         private float3 _leftMaxSteerPoint;
-        
-        public SplineContainer TrackSpline { set => trackSpline = value; }
 
-        public void OnCreate(Rigidbody rb, PlayerBoost boost, PlayerGround jump, PlayerAudio audioManager, UISpeedBar speedBar) {
+        public SplineContainer TrackSpline
+        {
+            set => trackSpline = value;
+        }
+
+        public void OnCreate(
+            Rigidbody rb,
+            PlayerBoost boost,
+            PlayerGround jump,
+            PlayerAudio audioManager,
+            UISpeedBar speedBar
+        )
+        {
             _rb = rb;
             _boost = boost;
             _ground = jump;
@@ -49,11 +73,18 @@ namespace Player {
             _speedUI = speedBar;
             _proximitySpeedBoost = GetComponent<ProximitySpeedBoost>();
         }
-        
-        public void OnFixedUpdate() {
-            if (!trackSpline || trackSpline.Spline == null) return;
 
-            SplineUtility.GetNearestPoint(trackSpline.Spline, transform.position, out _splineNearestPoint, out var t);
+        public void OnFixedUpdate()
+        {
+            if (!trackSpline || trackSpline.Spline == null)
+                return;
+
+            SplineUtility.GetNearestPoint(
+                trackSpline.Spline,
+                transform.position,
+                out _splineNearestPoint,
+                out var t
+            );
 
             // Compute frame from spline first
             _splineForward = math.normalize(trackSpline.Spline.EvaluateTangent(t));
@@ -63,52 +94,79 @@ namespace Player {
             _rightMaxSteerPoint = _splineNearestPoint + _splineRight * maxSteerLength;
             _leftMaxSteerPoint = _rightMaxSteerPoint + -2 * maxSteerLength * _splineRight;
         }
-    
-        private void OnCollisionEnter(Collision other) { 
-            if (((1 << other.gameObject.layer) & safetyMask.value) != 0) _forceRotation = false;
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (((1 << other.gameObject.layer) & safetyMask.value) != 0)
+                _forceRotation = false;
         }
 
-        private void OnCollisionExit(Collision other) {
-            if (((1 << other.gameObject.layer) & safetyMask.value) != 0) _forceRotation = true;
+        private void OnCollisionExit(Collision other)
+        {
+            if (((1 << other.gameObject.layer) & safetyMask.value) != 0)
+                _forceRotation = true;
         }
-        
-        public void Advance(float directionInput, bool brakeInput) {
-            if (_forceRotation) _rb.MoveRotation(Quaternion.FromToRotation(Vector3.forward, _splineForward));
-        
+
+        public void Advance(float directionInput, bool brakeInput)
+        {
+            if (_forceRotation)
+                _rb.MoveRotation(Quaternion.FromToRotation(Vector3.forward, _splineForward));
+
             var actualSpeed = GetEffectiveSpeed();
-            
+
             var steerVelocity = directionInput * playerSteerSpeed;
-            if (math.distance(_leftMaxSteerPoint, transform.position) >= maxSteerLength * 2 && steerVelocity > 0 || math.distance(_rightMaxSteerPoint, transform.position) >= maxSteerLength * 2 && steerVelocity < 0) {
+            if (
+                math.distance(_leftMaxSteerPoint, transform.position) >= maxSteerLength * 2
+                    && steerVelocity > 0
+                || math.distance(_rightMaxSteerPoint, transform.position) >= maxSteerLength * 2
+                    && steerVelocity < 0
+            )
+            {
                 steerVelocity = 0;
             }
 
             var horizontalVelocity = _splineForward * actualSpeed + _splineRight * steerVelocity;
-            if (brakeInput && !_boost.IsBoosted && _ground.IsGrounded()) horizontalVelocity = math.lerp(horizontalVelocity, float3.zero, brakeStrength * Time.fixedDeltaTime);
-        
+            if (brakeInput && !_boost.IsBoosted && _ground.IsGrounded())
+                horizontalVelocity = math.lerp(
+                    horizontalVelocity,
+                    float3.zero,
+                    brakeStrength * Time.fixedDeltaTime
+                );
+
             var currentVel = _rb.linearVelocity;
-            _rb.linearVelocity = new Vector3(horizontalVelocity.x, currentVel.y, horizontalVelocity.z);
-            if(math.distance(_splineNearestPoint, transform.position) >= maxSteerLength * 5) transform.position = _splineNearestPoint;
-            
-            _speedUI.UpdateSpeed(brakeInput ? 0 : actualSpeed,speed * boostSpeedMultiplier * (2 * proximityBoostMultiplier));
+            _rb.linearVelocity = new Vector3(
+                horizontalVelocity.x,
+                currentVel.y,
+                horizontalVelocity.z
+            );
+            if (math.distance(_splineNearestPoint, transform.position) >= maxSteerLength * 5)
+                transform.position = _splineNearestPoint;
+
+            _speedUI.UpdateSpeed(
+                brakeInput ? 0 : actualSpeed,
+                speed * boostSpeedMultiplier * (2 * proximityBoostMultiplier)
+            );
             _audio.SetSpeed(brakeInput ? 0 : actualSpeed * 100 / speed);
         }
-        
-        private float GetEffectiveSpeed() {
+
+        private float GetEffectiveSpeed()
+        {
             var current = speed;
-            
-            if (_boost && _boost.TryConsumeBoost()) current *= boostSpeedMultiplier;
+
+            if (_boost && _boost.TryConsumeBoost())
+                current *= boostSpeedMultiplier;
             current *= _proximitySpeedBoost.ProximityMultiplier() * proximityBoostMultiplier;
             return current;
         }
-    
-        private void OnDrawGizmos() {
+
+        private void OnDrawGizmos()
+        {
             Gizmos.color = Color.blue;
             Gizmos.DrawLine(_rightMaxSteerPoint, _leftMaxSteerPoint);
-            
+
             Gizmos.color = Color.red;
             Gizmos.DrawRay(transform.position, _splineForward);
             Gizmos.DrawLine(transform.position, _splineNearestPoint);
         }
-
     }
 }
