13cf5c85072ebe1b058fe80f88b8541c7689da76
TRISTAN <debeaunetristan@gmail.com>
2025-12-09 01:38:30 +0100

    add fail UI

--- Assets/Scripts/Player/PlayerTricks.cs
+++ Assets/Scripts/Player/PlayerTricks.cs
@@ -1,26 +1,41 @@
 using UI;
 using UnityEngine;
+using UnityEngine.Events;
 
-namespace Player {
+namespace Player
+{
     [RequireComponent(typeof(Animator))]
-    public class PlayerTricks : MonoBehaviour {
+    public class PlayerTricks : MonoBehaviour
+    {
+        public UnityEvent OnPlayerFailedTrick;
+
         private static readonly int IsGrounded = Animator.StringToHash("IsGrounded");
-        
-        private Animator _animator; 
-        private PlayerSyncActions _syncActions; 
+
+        private Animator _animator;
+        private PlayerSyncActions _syncActions;
         private UIScore _uiScore;
 
-        public void OnCreate(UIScore uiScore) {
+        public void OnCreate(UIScore uiScore)
+        {
             _uiScore = uiScore;
-            
+
             _animator = GetComponent<Animator>();
             _syncActions = GetComponentInParent<PlayerSyncActions>();
         }
 
         public void OnUpdate(bool isGrounded) => _animator.SetBool(IsGrounded, isGrounded);
-        
+
         public void LaunchTrick() => _animator.Play("Kickflip");
-        public void SucceedTrick() => _uiScore.IncreaseScore(_syncActions.SucceedTricks(gameObject.name == "Player 1" ? 0 : 1));
-        public void FailedTrick() => _uiScore.ResetScore();
+
+        public void SucceedTrick() =>
+            _uiScore.IncreaseScore(
+                _syncActions.SucceedTricks(gameObject.name == "Player 1" ? 0 : 1)
+            );
+
+        public void FailedTrick()
+        {
+            _uiScore.ResetScore();
+            OnPlayerFailedTrick.Invoke();
+        }
     }
 }
