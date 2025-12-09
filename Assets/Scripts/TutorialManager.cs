9e562baa50becd716af7fecd1da6b0efa83c15cc
theoffs <theofontany.pro@gmail.com>
2025-12-09 09:42:49 +0100

    add additive load

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -28,11 +29,19 @@ public class TutorialManager : MonoBehaviour {
 
     private void NextAnim(InputAction.CallbackContext obj) {
         if (++_state >= anims.Length) {
-            SceneManager.LoadScene("Game");
+            StartCoroutine(LoadGameAfterAdditive());
             return;
         }
         
         anims[_state - 1].SetActive(false);
         anims[_state].SetActive(true);
+    }
+
+    private static IEnumerator LoadGameAfterAdditive() {
+        var op = SceneManager.LoadSceneAsync("Circuit1_Art", LoadSceneMode.Additive);
+        
+        while (op is { isDone: false }) yield return null;
+        SceneManager.UnloadSceneAsync("Tuto");
+        SceneManager.LoadSceneAsync("Game", LoadSceneMode.Additive);
     }
 }
